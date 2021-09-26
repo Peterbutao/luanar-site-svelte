21b4a72d90e61abfae86480224f563f822c7436d
peterbutao <peterethanbutao@gmail.com>
2021-09-26 11:20:28 -1100

    initaial commit
    commit 1
    ..........

---
+++ src/App.svelte
@@ -0,0 +1,247 @@
+<script>
+	import Router, {link} from "svelte-spa-router";
+	import Home from "./routes/index.svelte";
+
+
+	$: open = false;
+</script>
+<Router routes={{
+	"/":Home
+}}/>
+
+
+
+<section  id="layout">
+    <nav class="nav-one">
+        <div class="nv">
+            <div on:click={() => {open = !open}} class="menu">
+                <div class="line-1"></div>
+                <div class="line-2"></div>
+                <div class="line-3"></div>
+            </div>
+            <div class="brand">
+                <a href="/">
+                    <span>             
+                        <img src="./assets/logo.png" width="50px" alt="logo">
+                    </span>
+                    <span>
+                        <h1>BSSY</h1>
+                    </span>    
+              </a>        
+            </div>
+            <div class:open={open} on:click={() => {open = !open}} class="nav-bar">
+                <ul on:click={() => {open = open}} class="navlist">
+                    <li><a href="https://github.com/Peterbutao/luanar-website-svelte.git">GET INVOLVED</a></li>
+                    <li><a href="https://github.com/Peterbutao/luanar-website-svelte.git">DOCS</a></li>
+                    <li><a href="https://github.com/Peterbutao/luanar-website-svelte.git/licence">LICENCE</a></li>
+                    <li><a href="tel://0880164455">CONTACT</a></li>            
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="content">
+        <slot></slot>
+    </main>
+    <footer class="foot">
+        <div class="ft">
+
+            <div class="brand">
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" viewBox="0 0 90 90"><g transform="translate(-17 -562)"><rect width="90" height="90" transform="translate(17 562)" fill="#d3dee7"/><text transform="translate(22 615)" fill="#2698fb93" font-size="24" font-family="SegoeUI-Bold, Segoe UI" font-weight="700"><tspan x="0" y="0">BUTAO</tspan></text></g></svg>
+                </span>
+                <span class="text">
+                    <h1>BUTAO DEV</h1>
+                    <p>&copy BUTAO UX / UI DEV | 2021</p>
+                </span>
+            </div>
+            <ul class="navlist">
+                <li><a href="https://github.com/Peterbutao/luanar-website-svelte.git">GET INVOLVED</a></li>
+                <li><a href="https://github.com/Peterbutao/luanar-website-svelte.git">DOCS</a></li>
+                <li><a href="https://github.com/Peterbutao/luanar-website-svelte.git">LICENCE</a></li>
+                <li><a href="tel://0880164455">CONTACT</a></li>            
+
+            </ul>
+            
+           
+
+
+        </div>
+    </footer>
+
+</section>
+
+<style lang="scss">
+    :global(body) {
+        color: blue;
+        background: var(--bc);
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    
+    :root{
+        --bc:#f5f5f5;
+        --tc:hsl(0, 0%, 25%);
+        --lc:seashell;
+        --bl:#2699fb;
+        --fb:#EBF1F6;
+        --fm: #d3dee7;
+        --tbc: #d3dee7;
+    }
+   
+    @mixin font($c, $s, $w) {
+        font-family:Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing:antialiased;
+        -moz-osx-font-smoothing:grayscale;
+        color:$c;
+        font-size: $s;
+        font-weight:$w;
+
+    }
+    #layout{
+        .nav-one{
+
+            z-index: 99;
+            position: fixed;
+            width: 100%;
+            background: var(--bc);
+            top: 0;
+            left: 0;
+            .nv{
+                box-shadow: #0000002c 0px 3px 11px 0px;
+                display: flex;
+                align-items: center;
+                background: var(--bc);
+                .menu{
+                    padding: 20px 10px;
+                    position: relative;
+                    z-index: 99;
+                    display: none;
+                    div{
+                        width: 20px;
+                        height: 3px;
+                        background: var(--tc);
+                     
+                    }
+                    .line-2{
+                        margin: 5px 0px;
+                        width: 12px;
+                    }
+                   
+                    
+                }
+                .brand{
+                    padding: 0px 10px;
+                    a{
+                        display: flex;
+                        align-items: center;
+                        padding: 3px 15px;
+                        text-decoration: none;
+                        h1{
+                            padding: 0px 15px;
+                            @include font(var(--tc), 1rem, 100);  
+                        }
+                    }
+                }
+                .open{
+                    width: 100% !important;
+                    transition: all 1s ease !important;
+                    opacity: 100% !important;
+                    pointer-events: all !important;
+                }
+                .nav-bar{
+                    z-index: 9;
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    background: #0000001e;
+                    opacity: 0;
+                    width:0;
+                    height: 100vh;
+                    overflow: hidden;
+                    padding: 0;
+                    transition: all 0.5s ease;
+                    pointer-events: none;
+
+                    .navlist{
+                    
+                        background: var(--fb);
+                        height: 100vh;
+                        width: 65%;
+                    }
+                    ul{
+                        margin: 0;
+                        padding: 20vh 0;
+                        li{
+                            list-style: none;
+                            padding: 20px 10px;
+                            a{
+                                text-decoration: none;
+                                @include font(var(--tc), 0.95rem, 500);
+                                
+                                span{
+                                    padding: 0px 5px;
+                                    svg{
+                                        filter: grayscale(50%);
+                                    }
+                                }
+                                
+                            }
+                            &:hover{
+                                background: #2699fb;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        .foot{
+            border-top: solid rgba(0, 0, 0, 0.309) 0.9px;   
+            position: relative;
+            bottom: 0;
+            height: 10vh;
+            
+            .ft{
+                padding: 0px 20px;
+                display: flex;
+                justify-content: space-between;
+                flex-direction:column;
+                ul{
+                        margin: 0;
+                        padding:0;
+                        padding-top: 20px;
+                        
+                        li{
+                            list-style: none;
+                            padding: 10px 0px;
+                        
+                            a{
+                                text-decoration: none;
+                                @include font(var(--tc), 0.95rem, 300);
+                                
+                            
+                                
+                            }
+                            
+                        }
+                    }
+                .brand{
+                    display: flex;
+                    .text{
+                        padding-left: 20px;
+                    }
+                    h1{
+                       @include font(var(--tc), 0.95rem, 800);
+                    }
+                    p{
+                        @include font(var(--bl), 0.7rem, 100)
+                    }
+                }
+               
+            }
+
+        }
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,85 @@
+<svelte:head>    
+    <title>luanar timetable | HOME</title>
+</svelte:head>
+<article>
+    <header>
+        <p>BSSY TIMETABLE FOR BSSY STUDENTS</p>
+    </header>
+    <main>
+        <h1>DOWNLOAD</h1>
+        <div class="links">
+            <div class="lnk">
+
+                <a href="./src" class="card">
+                    <div class="cd">
+                        <div class="text">
+                            <h1>
+                                <span>1.0.0</span>
+                                <span style="color: var(--bl)">LTS</span>
+                            </h1>
+                            <p>6/09/21</p>
+                        </div>
+                        <div class="svg">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="110" height="110" viewBox="0 0 110 110"><defs><clipPath id="a"><rect width="16" height="16" fill="none"/></clipPath></defs><g transform="translate(-259 -236)"><rect width="110" height="110" transform="translate(259 236)" fill="rgba(38,153,251,0.05)"/><g transform="translate(306 282)" clip-path="url(#a)"><path d="M8,0,6.545,1.455l5.506,5.506H0V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(16) rotate(90)" fill="rgba(38,153,251,0.53)"/></g></g></svg>
+
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </main>
+</article>
+
+<style lang="scss">
+        @mixin font($c, $s, $w) {
+        font-family:Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing:antialiased;
+        -moz-osx-font-smoothing:grayscale;
+        color:$c;
+        font-size: $s;
+        font-weight:$w;
+
+    }
+    article{
+        padding: 15vh 0;
+        padding-left: 10px;
+        padding-right: 10px;
+        background: var(--bc);
+        header{
+            p{
+                @include font(var(--tc), 0.95rem, 100);
+            }
+        }
+        main{
+            padding:20px 0px;
+            h1{
+                @include font(var(--tc), 0.95rem, 800);
+            }
+            .links{
+                .lnk{
+
+                    a{
+                        text-decoration: none;
+                        padding:0;
+                        margin:0;
+                        .cd{
+                            background: var(--fm);
+                            display: flex;
+                            align-items: center;
+                            justify-content: space-between;
+                            .text{
+                                padding: 10px;
+                                h1{
+                                    @include font(var(--tc), 1.5rem, 500);
+                                }
+                                p{
+                                    @include font(var(--tc), 0.7rem, 100);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
